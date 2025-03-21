4db8b93a7f0c701f36c971be6acf59870f9f39b3
kev405 <>
2025-03-21 16:15:22 -0500

    first commit

---
+++ src/main/java/com/codeflow/toolflow/ToolflowApplication.java
@@ -0,0 +1,13 @@
+package com.codeflow.toolflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToolflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToolflowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codeflow/toolflow/ToolflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeflow.toolflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToolflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
