85eff2c9844450fbe354bab56d6f3bad54a6b3f0
kev405 <>
2025-07-03 02:41:37 -0500

    Solucion de correo electronico (cambiar el correo del admin por el del usuario admin que se requiera al final)

--- src/main/java/com/codeflow/toolflow/service/email/EmailService.java
+++ src/main/java/com/codeflow/toolflow/service/email/EmailService.java
@@ -2,6 +2,7 @@ package com.codeflow.toolflow.service.email;
 import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
@@ -12,8 +13,12 @@ public class EmailService{
     @Autowired
     private JavaMailSender mailSender;
 
+    @Value("${spring.mail.from}")
+    private String fromEmail;
+
     public void sendSimpleEmail(String to, String subject, String text) {
         SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom(fromEmail);
         message.setTo(to);
         message.setSubject(subject);
         message.setText(text);
@@ -25,6 +30,7 @@ public class EmailService{
         MimeMessage message = mailSender.createMimeMessage();
         try {
             MimeMessageHelper helper = new MimeMessageHelper(message, true, "UTF-8");
+            message.setFrom(fromEmail);
             helper.setTo(to);
             helper.setSubject(subject);
             helper.setText(htmlBody, true);
